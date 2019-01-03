13e808b5aea3a072d2bf9538fd95b63976f43560
yaochenglong <289911401@qq.com>
2019-01-04 01:26:26 +0800

    1.修复查询日志bug

--- tax-model/src/main/java/com/chinasoft/tax/qo/LogInfoQo.java
+++ tax-model/src/main/java/com/chinasoft/tax/qo/LogInfoQo.java
@@ -2,10 +2,12 @@ package com.chinasoft.tax.qo;
 
 import com.chinasoft.tax.vo.LogInfoVo;
 import com.chinasoft.tax.vo.PageVo;
+import com.chinasoft.tax.vo.SearchVo;
 import lombok.Data;
 
 @Data
 public class LogInfoQo {
     PageVo pageVo;
     LogInfoVo logInfoVo;
+    SearchVo searchVo;
 }
